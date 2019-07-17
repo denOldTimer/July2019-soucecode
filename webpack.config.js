c3c024ed27f96f6b62e7de215f42e0af98082137
denOldTimer <kribo@outlook.com>
2019-07-17 08:48:07 +0200

    index

--- webpack.config.js
+++ webpack.config.js
@@ -84,6 +84,11 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        //font-awesome
+        test: /font-awesome\.config\.js/,
+        use: [{ loader: "style-loader" }, { loader: "font-awesome-loader" }]
       }
     ]
   },
